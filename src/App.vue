8e72e48bf8c0eb6d31f63db106774fd13ecab4a0
Ruhan Gupta <ruhan_gupta@outlook.com>
2023-09-16 23:55:10 -0500

    Fix gpa

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <!-- One or more icons in this file belong to Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 
 <template>
-  <main class="bg-zinc-900 w-full grid h-screen place-items-center">
+  <main class=" w-full grid h-[100vh] place-items-center">
     <div class="overflow-x-hidden w-full mx-auto flex items-center justify-center h-full">
       <div class="relative p-4 w-full lg:max-w-screen-xl h-full md:h-auto">
         <div
           class="relative p-4 bg-white rounded-lg dark:bg-gray-800 md:p-8 shadow-2xl shadow-[#0d817f]"
         >
           <div class="mb-4 text-sm font-light text-gray-500 dark:text-gray-400">
-            <h3 class="mb-3 text-4xl font-bold text-gray-900 dark:text-white">
+            <h3 class="mb-3 text-3xl lg:text-4xl font-bold text-gray-900 dark:text-white">
               RRISD GPA Calculator
             </h3>
             <p class="text-md font-semibold text-gray-700 dark:text-white mb-5 lg:mb-8">
@@ -49,26 +49,26 @@
                   ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                     <path
                       d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
-                    /></svg
-                ></span>
+                    />
+                  </svg></span>
                 New Course
               </button>
             </div>
             <div>
               <div class="rounded-md shadow-sm mr-2 p-2 text-center inline-block">
                 <p class="weighted-text text-4xl text-emerald-500">{{ weightedGPA.toFixed(3) }}</p>
-                <p class="text-white text-sm">Weighted GPA</p>
+                <p class="text-gray-800 dark:text-white text-sm">Weighted GPA</p>
               </div>
               <div class="rounded-md shadow-sm p-2 text-center inline-block">
                 <p class="text-4xl text-blue-500">{{ unweightedGPA.toFixed(3) }}</p>
-                <p class="text-white text-sm">Unweighted GPA</p>
+                <p class="text-gray-800 dark:text-white text-sm">Unweighted GPA</p>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <Footer />
+      </div>  
     </div>
-    <Footer />
     <SubjectSelectDropdown v-bind="dropdownOptions" @selected="onDropdownSelected" />
   </main>
 </template>
@@ -125,10 +125,9 @@ const weightedGPA = computed(() => {
 })
 
 const unweightedGPA = computed(() => {
-  const qualityPoints = relevantCourses.value.map(
-    (el) => (4 - (100 - el.average) / 10) * el.credits
-  )
-  return getGPA(qualityPoints)
+  const qualityPoints = relevantCourses.value.map((el) => (el.average >= 90 ? 4 : (4-(90-el.average)/10)))
+  return qualityPoints.reduce((prev, current) => prev + current, 0)/qualityPoints.length
+  
 })
 
 const currentSelectingCourse = ref(-1)
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,5 @@
 <template>
-  <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 fixed bottom-3">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center justify-center">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
         >Created by
         <a
@@ -32,5 +31,4 @@
             /></svg></span
       ></a>
     </div>
-  </footer>
 </template>
